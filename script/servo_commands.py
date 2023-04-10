2ce9b5488f99a24d3ea6474173190d69921043cf
wly <wlyzhcn@qq.com>
2023-04-10 16:36:50 +0800

    + real ackermann model (diff speed)

--- script/servo_commands.py
+++ script/servo_commands.py
@@ -5,7 +5,12 @@ from std_msgs.msg import Float32
 from std_msgs.msg import Float64
 from ackermann_msgs.msg import AckermannDriveStamped
 
+import math
+
 flag_move = 0
+
+d = 0.175
+l = 0.26*2
 
 def set_throttle_steer(data):
 
@@ -19,15 +24,28 @@ def set_throttle_steer(data):
     pub_pos_left_steering_hinge = rospy.Publisher('/ackermann_chassis/left_steering_hinge_position_controller/command', Float64, queue_size=1)
     pub_pos_right_steering_hinge = rospy.Publisher('/ackermann_chassis/right_steering_hinge_position_controller/command', Float64, queue_size=1)
 
-    throttle = data.drive.speed*31.25
-    steer = data.drive.steering_angle
+    # throttle = data.drive.speed*31.25
+    # steer = data.drive.steering_angle
 
-    pub_vel_left_rear_wheel.publish(throttle)
-    pub_vel_right_rear_wheel.publish(throttle)
-    pub_vel_left_front_wheel.publish(throttle)
-    pub_vel_right_front_wheel.publish(throttle)
+
+    throttle = data.drive.speed
+    steer = data.drive.steering_angle # angular velocity in fact
+
+    throttle_left = (throttle - steer * d) * 31.75
+    throttle_right = (throttle + steer * d) * 31.75
+
+    steer = math.atan2(steer*l, throttle)
+
+    pub_vel_left_rear_wheel.publish(throttle_left)
+    pub_vel_right_rear_wheel.publish(throttle_right)
+    pub_vel_left_front_wheel.publish(throttle_left)
+    pub_vel_right_front_wheel.publish(throttle_right)
     pub_pos_left_steering_hinge.publish(steer)
     pub_pos_right_steering_hinge.publish(steer)
+
+    rospy.loginfo("throttle_left: %f", throttle_left)
+    rospy.loginfo("throttle_right: %f", throttle_right)
+    rospy.loginfo("steer: %f", steer)
 
 def servo_commands():
 
